5ee4ee043e5e56ef86a96a16563b2a46a83e265c
B1g Digital <52285217+b1gDigital@users.noreply.github.com>
2023-01-11 08:57:07 -0500

    Add files via upload

---
+++ work-home.js
@@ -0,0 +1,144 @@
+
+    // Get the container element
+    const container = document.getElementById("containerBanner");
+    // Get the div elements inside the container
+    const div1 = document.getElementById("work-1");
+    const div2 = document.getElementById("work-2");
+    const div3 = document.getElementById("work-3");
+    const div4 = document.getElementById("work-4");
+    const div5 = document.getElementById("work-5");
+    const div6 = document.getElementById("work-6");
+    const div7 = document.getElementById("work-7");
+    const div8 = document.getElementById("work-8");
+    const div9 = document.getElementById("work-9");
+    const div10 = document.getElementById("work-10");
+    const div11 = document.getElementById("work-11");
+    const div12 = document.getElementById("work-12");
+    const div13 = document.getElementById("work-13");
+    const div14 = document.getElementById("work-14");
+    const div15 = document.getElementById("work-15");
+    const div16 = document.getElementById("work-16");
+    const div17 = document.getElementById("work-17");
+    const div18 = document.getElementById("work-18");
+    const div19 = document.getElementById("work-19");
+    const div20 = document.getElementById("work-20");
+    const div21 = document.getElementById("work-21");
+    const div22 = document.getElementById("work-22");
+    // Define a function that will be called when a div is clicked
+    function changeBackground(event) {
+    // Get the element that was clicked
+    const target = event.target;
+    // Update the background of the container based on the clicked element
+    if (target === div1) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01c3d429a1dce4179aa_AISH.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+} else if (target === div2) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0184849d26be3ee2f1a_Homi.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+} else if (target === div3) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00fe72150e338526ee9_Trailer%20Poker.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+} else if (target === div4) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01b5fb14314ab03bb1a_Pony-malta.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+} else if (target === div5) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01de8e40ec63354898d_Laika.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div6) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0115bed3754237846ec_BARK-BOX.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div7) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0115bed3724317846ed_Banco-de-bogota%CC%81.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div8) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0188042b08ff8dd4493_Piazza.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div9) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00d2d25e644b5204c1e_Grissly.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div10) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00b3d429a13744177bd_Papa%CC%81-heroe.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div11) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01b90461908210c1daf_Lafayette.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div12) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00d5a21e796d6ee2c5c_Corresponsal.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div13) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0134849d249cdee2f10_Procolombia%20-%20-origen-que-emociona.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div14) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01ae72150f49f526f85_Menticol.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div15) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde012e8e40e3d59547feb_Betplay.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div16) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde0118042b0708edd4475_VOGUE.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div17) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde01df4e575e6a4689d54_Poker-Ay.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div18) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00d38e6cd2a7c2f4cb6_Cafe%CC%81-Colombia.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div19) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00aedeae25846bb6465_Colanta.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div20) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00f5fb14304af03babb_Pepsi-tornamesa.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div21) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde00c4849d2236aee2ed2_finish.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}else if (target === div22) {
+    container.style.backgroundImage = "linear-gradient(180deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://uploads-ssl.webflow.com/63863035988e031353b3a32b/63bde013571e7c73d63fc241_Koleos.jpg')";
+    container.style.backgroundRepeat = "no-repeat";
+    container.style.backgroundSize = "cover";
+}
+}
+
+    div1.addEventListener("mouseover", changeBackground);
+    div2.addEventListener("mouseover", changeBackground);
+    div3.addEventListener("mouseover", changeBackground);
+    div4.addEventListener("mouseover", changeBackground);
+    div5.addEventListener("mouseover", changeBackground);
+    div6.addEventListener("mouseover", changeBackground);
+    div7.addEventListener("mouseover", changeBackground);
+    div8.addEventListener("mouseover", changeBackground);
+    div9.addEventListener("mouseover", changeBackground);
+    div10.addEventListener("mouseover", changeBackground);
+    div11.addEventListener("mouseover", changeBackground);
+    div12.addEventListener("mouseover", changeBackground);
+    div13.addEventListener("mouseover", changeBackground);
+    div14.addEventListener("mouseover", changeBackground);
+    div15.addEventListener("mouseover", changeBackground);
+    div16.addEventListener("mouseover", changeBackground);
+    div17.addEventListener("mouseover", changeBackground);
+    div18.addEventListener("mouseover", changeBackground);
+    div19.addEventListener("mouseover", changeBackground);
+    div20.addEventListener("mouseover", changeBackground);
+    div21.addEventListener("mouseover", changeBackground);
+    div22.addEventListener("mouseover", changeBackground);
